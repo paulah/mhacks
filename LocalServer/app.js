86ffe5ef997633cb40523e90631a0d0902c664ac
ALBERT ZHANG <rwzhang@umd.edu>
2015-01-17 16:10:59 -0500

    combine bargraph and app

--- LocalServer/app.js
+++ LocalServer/app.js
@@ -71,6 +71,73 @@ $(function() {
 		return map;
 	}
 
+	$(function () {
+    var callMap = getGraphStats();
+    var likeCount = callMap['happy'];
+    var dislikeCount = callMap['sad'];
+    var questionCount = callMap['question'];
+    var surpirseCount = callMap['exclamation'];
+
+
+    $('#container').highcharts({
+
+        
+
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'graph'
+        },
+        xAxis: {
+            categories: [
+                'current second',
+            ]
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Clicks'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} clicks</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: ':)',
+            data: [likeCount]
+
+        }, {
+            name: ':(',
+            data: [dislikeCount]
+
+        }, {
+            name: '?',
+            data: [questionCount]
+
+        }, {
+            name: '!',
+            data: [surpirseCount]
+
+        }]
+    });
+});
+
+
+
+
+
 	setTimeout(function () {
         var my_map = getGraphStats();
         alert(my_map['exclamation'] + ' should be 1');

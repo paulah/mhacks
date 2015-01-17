d76fa70b756c9b5978e0b3e57038abe0d6613884
ALBERT ZHANG <rwzhang@umd.edu>
2015-01-17 13:12:20 -0500

    simplifiedGraph

--- LocalServer/bargraph.js
+++ LocalServer/bargraph.js
@@ -1,25 +1,16 @@
 $(function () {
+
     $('#container').highcharts({
+
         chart: {
             type: 'column'
         },
         title: {
-            text: 'Clicks vs Time'
+            text: 'graph title'
         },
         xAxis: {
             categories: [
-                '1',
-                '2',
-                '3',
-                '4',
-                '5',
-                '6',
-                '7',
-                '8',
-                '9',
-                '10',
-                '11',
-                '12'
+                'current second',
             ]
         },
         yAxis: {
@@ -31,7 +22,7 @@ $(function () {
         tooltip: {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                '<td style="padding:0"><b>{point.y:.1f} clicks</b></td></tr>',
             footerFormat: '</table>',
             shared: true,
             useHTML: true
@@ -44,19 +35,19 @@ $(function () {
         },
         series: [{
             name: ':)',
-            data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+            data: [12]
 
         }, {
             name: ':(',
-            data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+            data: [18]
 
         }, {
             name: '?',
-            data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+            data: [15]
 
         }, {
             name: '!',
-            data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+            data: [16]
 
         }]
     });
